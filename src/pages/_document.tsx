d3940b4c8928746dbc4af4e91cc5723974f69d9f
toblaronesh <s.henderson1986@gmail.com>
2023-10-23 13:16:02 +1100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,8 +36,7 @@ class MyDocument extends Document {
             </>
           )}
           {/* google tag manager *} 
-          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{CONFIG.gtm.tagid}');`,
-    }}/>
+          
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
