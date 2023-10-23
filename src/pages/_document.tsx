1442485b7163e7ee89b764040edf6e83419aedbb
toblaronesh <s.henderson1986@gmail.com>
2023-10-23 02:55:10 +0000

    Remove GTM

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,26 +35,10 @@ class MyDocument extends Document {
               />
             </>
           )}
-         {/* google tag manager */}
-          {(process.env.NODE_ENV === 'production') &&
-        <script dangerouslySetInnerHTML={{
-            __html: `(function (w, d, s, l, i) {
-                    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                    var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                    j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', 'GTM-K55SWF47');`
-        }}></script>
-    }
-          
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
           <NextScript />
-          {/* google tag manager */}
-          {(process.env.NODE_ENV === 'production') &&
-        <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K55SWF47" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-        }}></noscript>}
         </body>
       </Html>
     )
