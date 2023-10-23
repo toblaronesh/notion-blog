f2c63df7da61c584fc26044c39061afedd8e2ff9
toblaronesh <s.henderson1986@gmail.com>
2023-10-23 12:53:55 +1100

    Update _document.tsx
    Added GTM to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,10 @@ class MyDocument extends Document {
               />
             </>
           )}
+          {/* google tag manager *} 
+          {<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{CONFIG.gtm.tagid'});</script>}
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
